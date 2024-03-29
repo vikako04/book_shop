0e9ee2a827aea26b0a19065b3ae1bb78c4f7c0b7
vikako04 <152388853+vikako04@users.noreply.github.com>
2024-03-29 21:21:38 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() 
-{
+document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('form');
     const searchInput = document.getElementById('search');
     const books = document.querySelectorAll('.book');
+    const container = document.querySelector('.container');
 
-    form.addEventListener('submit', function(event) 
-    {
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
         const searchTerm = searchInput.value.trim().toLowerCase();
+        let booksFound = false;
 
         books.forEach(function(book) {
             const title = book.querySelector('h2').textContent.trim().toLowerCase();
             const author = book.querySelector('p').textContent.trim().toLowerCase();
 
-            if (title.includes(searchTerm) || author.includes(searchTerm)) 
-            {
+            if (title.includes(searchTerm) || author.includes(searchTerm)) {
                 book.style.display = 'block';
-            } 
-            else 
-            {
+                booksFound = true;
+            } else {
                 book.style.display = 'none';
             }
         });
+
+        if (!booksFound) {
+            container.style.height = '315px';
+        } 
+        else 
+        {
+            container.style.height = 'auto';
+        }
     });
 });
-
