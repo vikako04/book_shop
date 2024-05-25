cbe9490a3ea6f83947e216122a24b4fdf2d4ce19
vikako04 <152388853+vikako04@users.noreply.github.com>
2024-05-25 10:43:14 +0500

    Delete src directory

--- src/main/java/org/example/projectspring/repository/AuthorCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.projectspring.repository;
-
-import org.example.projectspring.entity.AuthorCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorCategoryRepository extends JpaRepository<AuthorCategory, Integer> {
-}
